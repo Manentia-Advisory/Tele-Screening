8eb57102e9c67a2b89ea5a517b01de24f21c9edb
Somyaranjan Rout <somyaranjanmac1@gmail.com>
2021-09-18 22:22:56 +0530

    created image upload page and set up Router

--- src/components/image-upload.component.jsx
+++ src/components/image-upload.component.jsx
@@ -75,7 +75,7 @@ class ImageUplaod extends React.Component {
                     </button>
                     <button type="submit"
                         className="ml-3 bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Save
+                        Submit
                     </button>
                 </div>
             </div>
