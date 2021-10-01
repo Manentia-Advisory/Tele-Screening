4529c2eb8bab06f2f50d86f5635b9a3bc1fd6db0
chandbud5 <chandbud5@gmail.com>
2021-10-01 10:50:38 +0530

    DB models added

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import ImageTable, ContactUs
 # Register your models here.
+
+admin.site.register(ImageTable)
+admin.site.register(ContactUs)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+def processedColoredImageName(instance, filename):
+    return 'Image/{0}/processedColored.jpeg'.format(instance.ID)
+
+def UploadedImageName(instance, filename):
+    return 'Image/{0}/uploadedImage.jpeg'.format(instance.ID)
+
+def BoundingBoxImageName(instance, filename):
+    return 'Image/{0}/boundingBox.jpeg'.format(instance.ID)
+
+def ContactImage(instance, filename):
+    return 'ContactImage/{0}.jpeg'.format(instance.ID)
+
+class ImageTable(models.Model):
+    ID = models.IntegerField(default = int(datetime.now().timestamp() * 1000000), primary_key=True, editable=False)
+    CreatedAt = models.DateTimeField(auto_now=True)
+    DiseaseName = models.CharField(max_length = 512)
+    Percentage = models.CharField(max_length = 128)
+    UploadedImage = models.ImageField(upload_to = UploadedImageName)
+    ProcessedColoredImage = models.ImageField(upload_to = processedColoredImageName)
+    boundingBoxImage = models.ImageField(upload_to = BoundingBoxImageName)
+    Remarks = models.CharField(max_length = 512, blank=True)
+
+class ContactUs(models.Model):
+    ID = models.IntegerField(default = int(datetime.now().timestamp() * 1000000), primary_key = True, editable=False)
+    CreatedAt = models.DateTimeField(auto_now = True)
+    Email = models.EmailField()
+    Subject = models.CharField(max_length = 128)
+    Image = models.ImageField(upload_to = ContactImage, blank = True)
+    Bug_FeedBack_Query = models.CharField(max_length = 512)
